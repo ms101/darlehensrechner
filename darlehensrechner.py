3b9418c914d5281dd09a6590e7581dfc2a8c8923
ms101 <aass@abwesend.de>
2022-10-17 22:04:41 +0200

    Sondertilgung hinzugefügt +kleine fixes

--- darlehensrechner.py
+++ darlehensrechner.py
@@ -9,16 +9,17 @@
 import locale
 
 
-kredit = 112					# Darlehen in  Tausend €
-kredit *= 1000					# Kurzform von kredit = kredit * 1000
-sollzins = 0.75					# Sollzins in % pro Jahr
-zahlung = 4800					# Zahlungsrate/Annuität (Tilgung und Zins) pro Jahr
+kredit = 150					# Darlehen in Tausend €
+sollzins = 2.00					# Sollzins in % pro Jahr
+zahlung = 7500					# Zahlungsrate bzw. Annuität (Tilgung und Zins) pro Jahr
+sondertilgung = 7500				# jährliche Sondertilgung, gezahlt im Januar eines Jahres, oft 5 % vom kredit
 zperiode = 12					# Zahlungsperiode, x mal im Jahr
-skip = 5					# alle skip Jahre Werte ausgeben
-graphZeigen = False				# Graph ausgeben
+skip = 2					# alle skip Jahre Werte ausgeben
+graphZeigen = False				# Graph ausgeben (True oder False)
+
 if graphZeigen:
 	import matplotlib.pyplot as plot
-schulden = kredit				# restliche Schulden, Anfangswert ist der Kredit
+schulden = kredit * 1000			# restliche Schulden, Anfangswert ist der Kredit Faktor Tausend
 ezins = (1 + sollzins/zperiode) ** zperiode -1	# effektiver Jahreszins bei monatlicher Zahlung
 tilgung = 0					# berechneter Tilgungsanteil je Monat
 jahre = 0					# berechnete Gesamtlaufzeit
@@ -35,35 +36,48 @@ print("┌────────────────── Darlehensrechne
 print("\tKredit:\t\t\t" + euro(kredit))
 print("\tAnnuität:\t\t" + euro(zahlung) + "/a")
 print("\t\t\t\t" + euro(zahlung/12) + "/Monat")
+if sondertilgung > 0:
+	print("\tSondertilgung:\t\t" + euro(sondertilgung) + "/a")
 print("\tNominalzins:\t\t" + str(sollzins) + " %/a")
 print("\tEff. Jahreszins:\t" + str(round(ezins, 2)) + " %/a")
 print("├──────────────────────────────────────────────────────┤")
 
 while schulden > 0:				# while-Schleife bis es keine Schulden mehr gibt
-	jahre += 1				# Jahre hochzählen (jahre = jahre +1)
-	zins = schulden * sollzins/100		# Zins für Jahr berechnen
-	zsum += zins				# Zins aufsummieren
-	tilgung = zahlung - zins		# Tilgung für Jahr berechnen
-	if tilgung < 0:				# Sonderfall zu geringe Zahlungsrate
-		print("[!] Zahlungsrate zu gering: Zins übersteigt Zahlungsrate.")
-		exit(1)
+	jahre += 1				# Jahre um eins hochzählen
+	if schulden < sondertilgung:		# letztes Jahr Resttilgung durch Sondertilgung
+		sondertilgung -= schulden
+		schulden = 0
+		zins = 0
+		tilgung = 0
+	else:
+		schulden -= sondertilgung		# jährliche Sondertilgung, am Anfang vom Jahr
+		zins = schulden * sollzins/100		# Zins für Jahr berechnen
+		zsum += zins				# Zins aufsummieren
+		tilgung = zahlung - zins		# Tilgung für Jahr berechnen
+		if tilgung < 0:				# Sonderfall zu geringe Zahlungsrate
+			print("[!] Zahlungsrate zu gering: Zins übersteigt Zahlungsrate.")
+			exit(1)
+
 	if schulden < tilgung:			# Sonderfall letztes Jahr, komplette Tilgung
+		tilgung = schulden		# letztes Jahr wird nicht monategenau berechnet (Zins)
 		schulden = 0
 	else:
 		schulden -= tilgung		# Restkredit für nächstes Jahr berechnen
 	
 	# Werte ausgeben alle $skip Jahre und immer das erste und letzte Jahr
-	if skip == 1 or jahre % skip == 1 or schulden == 0:
+	if jahre % skip == 1 or skip == 1 or schulden == 0:
 		zlist.append(zins)
 		tlist.append(tilgung)
 		jlist.append("Jahr " + str(jahre))
 		print("Jahr " + str(jahre))
 		print("\tZins:\t\t" + euro(zins) + ("\t\t" if zins<100 else "\t") + euro(zins/12) + "/Monat")
 		print("\tTilgung:\t" + euro(tilgung) + ("\t\t" if tilgung<100 else "\t") + euro(tilgung/12) + "/Monat")
+		if sondertilgung > 0:
+			print("\tSondertilgung:\t" + euro(sondertilgung) + ("\t\t" if tilgung<100 else "\t"))
 		print("\tRestkredit\t" + euro(schulden))
 
 print("├──────────────────────────────────────────────────────┤")
-print("\tLaufzeit:\t\t" + str(jahre) + " Jahre")
+print("\tLaufzeit:\t\t< " + str(jahre) + " Jahre")
 print("\tZinszahlungen:\t\t" + euro(zsum) + " (" + str(round(100/kredit*zsum, 2)) + " %)")
 print("└──────────────────────────────────────────────────────┘")
 
